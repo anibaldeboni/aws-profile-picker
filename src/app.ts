848aec1f6c950c7ee81c828b2aa93f5562892d99
Aníbal Deboni Neto <anibaldeboni@gmail.com>
2021-12-10 15:15:07 -0300

    add run k9s

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import os from 'os';
 import fs from 'fs';
 import prompts from 'prompts';
-import { spawn } from 'child_process';
+import { spawn, exec } from 'child_process';
 
 const HOME = os.homedir();
 const awsConfigFile = `${HOME}/.aws/config`;
 
 const readConfigFile = (path: string) => fs.readFileSync(path);
 
-const getProfileNames = (configFile: Buffer) => {
+const getProfileNames = (configFile: Buffer): string[] => {
   const profileNamesRegex = /(?<=profile\s)[\w-_]+/g;
   const profiles = configFile.toString().match(profileNamesRegex);
   if (!profiles) throw Error('No profiles found');
@@ -18,18 +18,18 @@ const getProfileNames = (configFile: Buffer) => {
 const buildPromptMenu = (choices: string[]) => choices.map((choice) => ({ title: choice, value: choice }));
 
 const run = (command: string, options: string[]): Promise<number> => new Promise((resolve, reject) => {
-  const exec = spawn(command, options);
-  exec.stdout.on('data', (data) => console.log(data.toString()));
-  exec.stderr.on('data', (data) => console.log(data.toString()));
+  const app = spawn(command, options);
+  app.stdout.on('data', (data) => console.log(data.toString()));
+  app.stderr.on('data', (data) => console.log(data.toString()));
 
-  exec.on('close', (code) => {
+  app.on('close', (code) => {
     if (code !== 0) reject(1);
     resolve(0);
   });
 });
 
 (async () => {
-  const response = await prompts(
+  const { profile } = await prompts(
     {
       type: 'select',
       name: 'profile',
@@ -42,5 +42,23 @@ const run = (command: string, options: string[]): Promise<number> => new Promise
     },
   );
 
-  await run('aws', ['sso', 'login', '--profile', response.profile]);
+  const sso = await run('aws', ['sso', 'login', '--profile', profile]);
+
+  if (sso !== 0) throw new Error('AWS SSO terminated unexpectedly');
+
+  const { runK9s } = await prompts(
+    {
+      type: 'toggle',
+      name: 'runK9s',
+      message: 'Wish to run k9s?',
+      initial: true,
+      active: 'yes',
+      inactive: 'no',
+    },
+  );
+
+  if (runK9s) {
+    console.info('Running k9s...');
+    exec('k9s').unref();
+  }
 })();
